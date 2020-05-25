179fe68cca913956942735b7abd90dd4ac06ddc1
Reformed Scribe <stmiller57@gmail.com>
2020-05-25 11:48:33 -0400

    created orm.js file

---
+++ config/orm.js
@@ -0,0 +1,84 @@
+// Import MySQL connection.
+const connection = require("../config/connection.js");
+
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+    let arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    };
+
+    return arr.toString();
+};
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    let arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (let key in ob) {
+        let value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            };
+            arr.push(key + "=" + value);
+        };
+    };
+    return arr.toString();
+}
+
+// Object for all our SQL statement functions.
+const orm = {
+    selectAll: (table, cb) => {
+        let queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: (table, cols, vals, cb) => {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    // An example of objColVals would be {name: cheeserburger, devoured: false}
+    updateOne: (table, objColVals, condition, cb) => {
+        let queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const express = require("express");
-const mysql = require("mysql");
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Tatum!Football2House",
-    database: "burgers_db"
-});
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Initiate MySQL Connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+// Set Handlebars.
+const exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Start our server so that it can begin listening to client requests.
+// Import routes and give the server access to them.
+const routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
 app.listen(PORT, () => {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+    console.log("App now listening at localhost:" + PORT);
 });
