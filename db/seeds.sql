eefefeed4857fea2aa6d2d7c088f4edeee0c1f6f
Reformed Scribe <stmiller57@gmail.com>
2020-05-25 11:23:44 -0400

    Updated .sql files and server.js file for database construction

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ bacon cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('mushroom swiss burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('jalapeno pepperjack burger', false);
--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-import express from 'express';
+const express = require("express");
+const mysql = require("mysql");
 const app = express();
-const port = 3000
+
+const PORT = process.env.PORT || 8080;
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Tatum!Football2House",
+    database: "burgers_db"
+});
+
+// Initiate MySQL Connection.
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () => {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
