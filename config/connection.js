040e9e0b6048226b672aedb5eb5b1ac93460cb97
Reformed Scribe <stmiller57@gmail.com>
2020-05-27 13:26:48 -0400

    Adding ability to host on Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 // Set up MySQL connection
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Tatum!Football2House",
-    database: "burgers_db"
-});
-
+const connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Tatum!Football2House",
+        database: "burgers_db"
+    });
+};
 // Make connection
 connection.connect((err) => {
     if (err) {
